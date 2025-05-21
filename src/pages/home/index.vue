aeb35de4b92e21db85a53470dc05d3bd96da458e
Ahmed-Roshdy-1 <ahmedroshdy01029@gmail.com>
2025-05-21 21:07:43 +0300

    add images data and initialize home page with some style

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,22 @@
-<script setup>
-import HomeHeader from './partials/header.vue'
+<script setup lang="ts">
+import { ref, onMounted,onUnmounted } from 'vue';
+import HomeHeader from './partials/header.vue';
+
+const images = [
+    'images/slide-01.jpg',
+    'images/slide-02.jpg',
+    'images/slide-03.jpg'
+];
+
 </script>
 
 <template>
-    <home-header />
+    <div class="hero">
+        <div class="hero-images" v-for="(image, index) in images" :key="image">
+            <img class="hero-img" :src="image" :alt="`background ${index + 1}`" />
+        </div>
+
+        <!-- I positioned the header here to be above the images without using z-index -->
+        <home-header />
+    </div>
 </template>
