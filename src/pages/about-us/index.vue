daf71bfc8a5166eabbcff2aec6b71292802435fc
Ahmed-Roshdy-1 <ahmedroshdy01029@gmail.com>
2025-06-14 19:29:28 +0300

    add card section

--- src/pages/about-us/index.vue
+++ src/pages/about-us/index.vue
@@ -1,63 +1,21 @@
 <script lang="ts" setup>
+import mainSection from './partials/main-section.vue';
+import Card from './partials/card.vue';
 </script>
 <template>
     <div id="about-us" class="about-us">
         <div class="title">
             <h1>About Us</h1>
         </div>
-        <main>
-            <div class="left">
-                <h2>I work with top brands and startups</h2>
-                <div class="left-icons">
-                    <div class="row">
-                        <div class="left-icon-item">
-                            <img src="/images/service-item-01.png" alt="">
-                            <p>Top Notch</p>
-                        </div>
-                        <div class="left-icon-item">
-                            <img src="/images/service-item-01.png" alt="">
-                            <p>Top Notch</p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="left-icon-item">
-                            <img src="/images/service-item-01.png" alt="">
-                            <p>Top Notch</p>
-                        </div>
-                        <div class="left-icon-item">
-                            <img src="/images/service-item-01.png" alt="">
-                            <p>Top Notch</p>
-                        </div>
-                    </div>
-                </div>
-                <button>Learn More &rarr;</button>
-            </div>
-            <div class="right">
-                <div>
-                    <p>
-                        <a href="#">Breazed with Vue</a>
-                        Today’s web is highly interactive, dynamic, and personalized. Modern websites are often powered
-                        by frameworks and libraries such as React, Vue.js, or Angular, and many are backed by databases
-                        and APIs.
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        Technologies like HTML5, CSS3, and JavaScript enable developers to build sophisticated user
-                        experiences that work across a wide range of devices and platforms.
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        At its core, the Web operates on a client-server model. Web browsers act as clients that request
-                        resources (like HTML, CSS, and JavaScript files) from web servers. These servers respond by
-                        sending the requested content, which the browser then renders into a visual web page.
-                        <a href="#">The Hypertext Transfer Protocol (HTTP)</a> and its secure variant (HTTPS) are the
-                        foundations of this communication.
-                    </p>
-                </div>
-            </div>
-        </main>
+        <main-section />
+
+        <hr style="border: 1px solid #c2c2c299; width: 80%; margin: 1rem auto;">
+
+        <div class="cards-section">
+            <card />
+            <card />
+            <card />
+        </div>
     </div>
 
 </template>
---
+++ src/pages/about-us/partials/card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="card">
+        <img src="/images/features-icon-1.png" alt="">
+        <div class="card-body">
+            <h3>Web Performance</h3>
+            <p>At its core, the Web operates on a client-server model. Web browsers act as clients that request resources
+                (like HTML, CSS, and JavaScript files) from web servers. These servers respond by sending the requested
+                content, which the browser then renders into a visual web page.</p>
+            <a href="#">Learn More &rarr;</a>
+
+        </div>
+    </div>
+</template>
