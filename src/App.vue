60cb11cfecefebc80c585a3d3ccb858a930bee6b
Ahmed-Roshdy-1 <ahmedroshdy01029@gmail.com>
2025-06-25 19:27:36 +0300

    Add newsletter component and styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import HomePage from './pages/home/index.vue'
 import AboutPage from './pages/about-us/index.vue'
+import Newsletters from './components/newsletters.vue';
 
 </script>
 
 <template>
   <home-page />
   <about-page />
+  <newsletters />
 </template>
 
 
---
+++ src/components/newsletters.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <div class="newsletter">
+        <div class="newsletter-title">
+            <h1>Subscribe to our newsletter</h1>
+        </div>
+        <div class="newsletter-input">
+            <input type="email" placeholder="Email Address">
+            <button>Subscribe</button>
+        </div>
+    </div>
+</template>
