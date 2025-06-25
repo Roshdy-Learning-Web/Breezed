f040d59aa7878f49fbd498f78b28d20c5c425cef
Ahmed-Roshdy-1 <ahmedroshdy01029@gmail.com>
2025-06-25 19:44:44 +0300

    Update newsletter component layout and styles

--- src/components/newsletters.vue
+++ src/components/newsletters.vue
@@ -4,11 +4,16 @@
 <template>
     <div class="newsletter">
         <div class="newsletter-title">
-            <h1>Subscribe to our newsletter</h1>
+            <h3>Subscribe to our newsletter</h3>
+            <h4>Don’t miss this chance!</h4>
+            <p>
+                Stay updated with the latest news and offers from our team. Sign up for our newsletter to stay up to date
+                with our latest products.
+            </p>
         </div>
-        <div class="newsletter-input">
+        <form class="newsletter-input" @submit.prevent>
             <input type="email" placeholder="Email Address">
-            <button>Subscribe</button>
-        </div>
+            <button type="submit">Subscribe</button>
+        </form>
     </div>
 </template>
